83783054872d477ffa0316e4a60879321f33ebba
ariefardi <forzaariefpyon@gmail.com>
2018-04-03 12:43:34 +0700

    lupa commit tadi, ini udah selesai susun kursi

--- susunKursi.js
+++ susunKursi.js
@@ -12,29 +12,36 @@
 
 function generateSeats(row) {
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for(let i=0; i<row; i++) {
+		seats.push([])
 	}
+	// console.log('ini seats ',seats);
+	// console.log('ini row ',row);
 	return seats
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	let seats = generateSeats(rowSeats)
 	let row = 0
-
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
+// console.log('ini arr ',arr);
+// console.log('ini rowSeats ',rowSeats);
+	for(let i=0; i<arr.length; i++) {
+		//0,3,6,9
+		//1,4,7
+		//2,5,8
+		seats[row].push(arr[i])
+		if(row < 2) {
 			row++
 		} else {
-			row == 0
+			row = 0
 		}
 	}
+	//console.log('ini seats ',seats);
 	printSeats(seats)
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
+	for(let i=0; i<3; i++) {
 		console.log(`Baris ${i} : `, seats[i])
 	}
 }
